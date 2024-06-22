c91869f680b3a1d3cfb55247c7aeca663432e622
justinp24 <justinpardo2423@gmail.com>
2024-06-22 17:04:12 -0400

    limit amount of cards displayed to player at once

--- my-first-react-app/src/Components/Game.jsx
+++ my-first-react-app/src/Components/Game.jsx
@@ -1,6 +1,7 @@
 import "../App.css";
 import img from "../assets/logo.png";
 import { useState, useEffect } from "react";
+import { v4 as uuidv4 } from "uuid";
 
 import img1 from "./img/jin.png";
 import img2 from "./img/brook.png";
@@ -23,12 +24,29 @@ export default function Game({ gameToggle, gameDif }) {
         initalizeCards();
     }, [gameDif]);
 
+    function shuffleCards() {
+        const shuffledCards = [...cards];
+        for (let i = shuffledCards.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [shuffledCards[i], shuffledCards[j]] = [
+                shuffledCards[j],
+                shuffledCards[i],
+            ];
+        }
+        setCards(shuffleCards);
+    }
+
     function toggleCard(index) {
+        // when i click a card it should randomly shuffle the cards
+        // this should cause a rerender automtically since there state variables
+        // also have the logic to check if the card was already true end game and show restart div
         let updatedCards = [...cards];
         updatedCards[index].value = !updatedCards[index].value;
         setCards(updatedCards);
     }
+    function generateId() {}
     const initalizeCards = () => {
+        // make it so it also generates an id for each card
         const imgArray = [
             img1,
             img2,
@@ -42,18 +60,23 @@ export default function Game({ gameToggle, gameDif }) {
             img10,
         ];
         let numberOfCards;
+        let displayedAmount;
         if (gameDif === "Easy") {
             numberOfCards = 5;
+            displayedAmount = 3;
         }
         if (gameDif === "Medium") {
             numberOfCards = 7;
+            displayedAmount = 4;
         }
         if (gameDif === "Hard") {
             numberOfCards = 10;
+            displayedAmount = 5;
         }
         const initialCards = imgArray.slice(0, numberOfCards).map((src) => ({
             src: src,
             value: false,
+            displayedAmount: displayedAmount,
         }));
         setCards(initialCards);
     };
@@ -70,15 +93,31 @@ export default function Game({ gameToggle, gameDif }) {
                 </div>
             </div>
             <div className="card-container">
-                {cards.map((card, index) => (
-                    <Card
-                        key={index}
-                        src={card.src}
-                        value={card.value}
-                        index={index}
-                    />
-                ))}
+                {cards.map(
+                    (card, index) =>
+                        index < card.displayedAmount && (
+                            <Card
+                                key={index}
+                                src={card.src}
+                                value={card.value}
+                                index={index}
+                            />
+                        )
+                )}
             </div>
         </div>
     );
 }
+
+// so now you have to make it when it renders card it renders any random cards from
+// the card state array and no matter the difficulty only 5 at a time
+// when you click on a card it will trigger the function passed down from game into
+// card which will return the index assocaited with said event and tirgger a update the card
+// and trigger a re render and shift order all random but at most 1 whose value is true
+
+// new way
+// just when clicked
+// on a card do the thing to turn its value to true and check it
+// was already true or else game over
+// randomly shuffle cards and there should not be that edge case scenario
+// instead of making key the index use uuid
